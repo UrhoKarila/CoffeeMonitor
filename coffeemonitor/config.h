6366ee61ff6fb20c92afe861a6aa274ce913bffc
Grant Ebeling <gebeling@ostusa.com>
2018-10-02 13:57:26 -0500

    Added basic OLED functionality Organized libraries to work properly

--- coffeemonitor/config.h
+++ coffeemonitor/config.h
@@ -1,14 +1,20 @@
-#define USE_RGB_INDICATOR
+//#define DEBUG
+
+//#define USE_RGB_INDICATOR
 #ifdef USE_RGB_INDICATOR
   #define PIN_BLUE	0
   #define PIN_GREEN	0
   #define PIN_RED	0
 #endif //USE_RGB_INDICATOR
 
+#define UPDATE_FREQ 1
+
 #define USE_OLED_INDICATOR
 #ifdef USE_OLED_INDICATOR
 //may depend on individual screen. Use an I2C scanner to determine your address.
-	#define I2C_ADDRESS
-	#define PIN_SCL 5
-	#define PIN_SDA 6
+	#define I2C_ADDRESS 0x3C
+	//#define GND 0
+	//#define VCC 2
+	//#define PIN_SCL 3
+	//#define PIN_SDA 4
 #endif
